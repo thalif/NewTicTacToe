7b778a95a588be1013d580cd252a95becdc86023
thalif <thalif12@gmail.com>
2021-03-06 16:11:18 +0530

    Add project files.

---
+++ NewTicTacToe/GameValidation.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTicTacToe
+{
+    public class GameValidation
+    {
+        public bool ValidationProcess(string[] arr)
+        {
+            if (LeftVerticalCheck(arr))
+                return true;
+            if (RightVerticalCheck(arr))
+                return true;
+            if (MiddleVerticalCheck(arr))
+                return true;
+
+            if (TopHorizontalCheck(arr))
+                return true;
+            if (MiddleHorizontalCheck(arr))
+                return true;
+            if (BottomHorizontalCheck(arr))
+                return true;
+
+            if (LeftToRightDiagnol(arr))
+                return true;
+            if (RightToLeftDiagnol(arr))
+                return true;
+            return false;
+        }
+
+        public bool IsDraw(string[] arr)
+        {
+            if (!arr.Contains("_"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Match Draw");
+                Console.ResetColor();
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckEmptyValues(string v1, string v2, string v3)
+        {
+            if (v1 != "_" && v2 != "_" && v3 != "_")
+                return true;
+            else
+                return false;
+        }
+
+        public bool LeftVerticalCheck(string[] arr)
+        {
+            string v1 = arr[0];
+            string v2 = arr[3];
+            string v3 = arr[6];
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool RightVerticalCheck(string[] arr)
+        {
+            string v1 = arr[2];
+            string v2 = arr[5];
+            string v3 = arr[8];
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool MiddleVerticalCheck(string[] arr)
+        {
+            string v1 = arr[1];
+            string v2 = arr[4];
+            string v3 = arr[7];
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool TopHorizontalCheck(string[] arr)
+        {
+            string v1 = arr[0];
+            string v2 = arr[1];
+            string v3 = arr[2];
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool MiddleHorizontalCheck(string[] arr)
+        {
+            string v1 = arr[3];
+            string v2 = arr[4];
+            string v3 = arr[5];
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool BottomHorizontalCheck(string[] arr)
+        {
+            string v1 = arr[6];
+            string v2 = arr[7];
+            string v3 = arr[8];
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool LeftToRightDiagnol(string[] arr)
+        {
+            string v1 = arr[0];
+            string v2 = arr[4];
+            string v3 = arr[8];
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool RightToLeftDiagnol(string[] arr)
+        {
+            string v1 = arr[2];
+            string v2 = arr[4];
+            string v3 = arr[6];
+            if (v1 == v2 && v2 == v3 && CheckEmptyValues(v1, v2, v3))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player\t" + v1 + "\tWon Game");
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ NewTicTacToe/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTicTacToe
+{
+    class Program
+    {
+        static public string[] PlayBoard = new string[9] { "_", "_", "_", "_", "_", "_", "_", "_", "_" };
+        static public List<int> PlayerHit = new List<int>();
+        static bool Player1 = false;
+        static bool Player2 = false;
+        static void Main(string[] args)
+        {
+            Toss();
+            LoadBoard();
+            GameStart();
+
+            Console.ReadKey();
+        }
+        static public void GameStart()
+        {
+            GameValidation gv = new GameValidation();
+            for (int i = 0; i < 9; i++)
+            {
+                if (Player1)
+                    i -= Player1Input();
+                else if (Player2)
+                    i -= Player2Input();
+
+                if (i >= 4)
+                    if (gv.ValidationProcess(PlayBoard))
+                        break;
+                    else if(i == 8)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Draw");
+                        Console.ResetColor();
+                        break;
+                    }
+            }
+        }
+        static public void Toss()
+        {
+            Random random = new Random();
+            int ran = random.Next(0, 2);
+            if (ran == 0)
+            {
+                Player1 = true;
+                Console.WriteLine("Player X to Start");
+            }
+            else if (ran == 1)
+            {
+                Player2 = true;
+                Console.WriteLine("Player O to Start");
+            }   
+        }
+
+        static public bool CheckIsMatchDraw(string[] arr)
+        {
+            string PreviousValue = "";
+            bool IsMatchDraw = false;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == "_")
+                {
+                    if (PreviousValue == arr[i])
+                    {
+                        Console.WriteLine("Match Draw");
+                        IsMatchDraw = true;
+                        break;
+                    }
+                    else
+                        PreviousValue = arr[i];
+                }
+                else
+                    PreviousValue = arr[i];
+            }
+            return IsMatchDraw;
+        }
+
+        static public void LoadBoard()
+        {
+            Console.Write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            for (int i = 0; i < PlayBoard.Length; i++)
+            {
+                if (PlayBoard[i] == "X")
+                    Console.ForegroundColor = ConsoleColor.Red;
+                else if(PlayBoard[i] == "O")
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                else
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                Console.Write(PlayBoard[i] + "\t\t\t\t");
+                if (i == 2 || i == 5 || i == 8)
+                    Console.Write("\n\n\n\n");
+            }
+        }
+
+        static public int Player1Input()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Player\tX\t|");
+            Console.ResetColor();
+            string a = Console.ReadLine();
+            int input = int.Parse(a);
+
+            if (!PlayerHit.Contains(input))
+            {
+                PlayerHit.Add(input);
+                PlayBoard[input - 1] = "X";
+                LoadBoard();
+                Player1 = false;
+                Player2 = true;
+                return 0;
+            }
+            else
+                return 1;
+        }
+        static public int Player2Input()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Player\tO\t|");
+            Console.ResetColor();
+            string a = Console.ReadLine();
+            int input = int.Parse(a);
+
+            if (!PlayerHit.Contains(input))
+            {
+                PlayerHit.Add(input);
+                PlayBoard[input - 1] = "O";
+                LoadBoard();
+                Player1 = true;
+                Player2 = false;
+                return 0;
+            }
+            else
+                return 1;
+        }
+
+        
+    }
+}
